5c206610a6df30ef9a701ba4a3b15ecbc72a5cb3
ACS <sahin.ahmetcemal@gmail.com>
2021-02-19 16:23:08 -0800

    Constants

---
+++ Great Wisdom/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  constants.swift
+//  Great Wisdom
+//
+//  Created by ACS on 19.02.2021.
+//
+
+import Foundation
+
+let kString_appName = "Great Wisdom"
+let kString_home = "home"
+let kString_favourites = "favourites"
+let kString_about = "about"
+let kString_other = "other"
+let kString_moreApps = "moreApps"
+
+let kIconName_app = "Quote"
+
+let kIconName_home = "house.fill"
+let kIconName_favourites = "star.fill"
+let kIconName_about = "info.circle.fill"
+let kIconName_moreApps = "apps.iphone"
+
+let kImageName_sideMenuHeader = "Dostoevsky"
+
+let kName_sideMenu_tableViewCell = "TableViewCell"
+let kIdentifier_sideMenu_tableCellIdentifier = "ReusableCell"
+
+let kUrlString_moreApps = "https://play.google.com/store/apps/details?id=com.acs.tabuu_app"
+
+let kIdentifier_segue_toMoreApps = "goToMoreApps"
+let kIdentifier_segue_toSearch = "goToSearch"
+
+let kIdentifier_searchView_tableCellIdentifier = "searchCell"
